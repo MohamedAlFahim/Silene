7ddaac9b54a60c2370a2ef22dc2ee5cdd357ba00
MohamedAlFahim <malfahim8@gmail.com>
2020-06-12 13:05:47 -0700

    Continue working on Analyzer.

--- silene/analyzer.py
+++ silene/analyzer.py
@@ -64,11 +64,21 @@ class Transition(typing.NamedTuple):
 
 
 class Analyzer:
+    """
+    Stores transition logic. Calling a code generator function on it will generate the code necessary for a lexer.
+    """
+
     def __init__(self, *, num_of_states: int):
+        """
+        Creates an Analyzer with the specified number of states.
+
+        :param num_of_states: The number of states the lexer has.
+        """
         self.num_of_states = num_of_states
-        # An index represents a 'from_st' state, and the value at that index is
-        # a list of transitions that originate from that 'from_st' state
-        self.transitions = [[] for i in range(num_of_states)]
+
+        # An index represents a 'from_st' state, and the value at that index is a list of transitions that originate
+        # from that 'from_st' state.
+        self.transitions = [[] for _ in range(num_of_states)]
 
     def append_transition_from_state(self, *, if_char_logic: IfCharLogic,
                                      actions: typing.List[Action], to_st: int,
@@ -77,18 +87,29 @@ class Analyzer:
             if_char_logic=if_char_logic, actions=actions, to_st=to_st))
 
     def add_transition(self, *, from_st: int, to_st: int, if_char,
-                       actions: typing.List[str] = []):
+                       actions: typing.Optional[typing.List[str]] = None):
+        """
+        Adds a transition from one state to either another state or the same state. The transition occurs if the
+        specified condition is satisfied. In addition, a set of actions will be taken.
+
+        :param from_st: The starting state of the transition.
+        :param to_st: The ending state of the transition.
+        :param if_char: The condition that must be satisfied.
+        :param actions: The set of actions to take.
+        :return:
+        """
+        actions: typing.List[str] = actions or []
         max_possible_state_num = self.num_of_states - 1
-        action_list = []
+        action_list: typing.List[Action] = []
+
         # Range check from_st state number and to_st state number
         if (from_st < 0) or (from_st > max_possible_state_num):
-            raise IndexError(f'The "from" state {from_st} is not a possible '
-                             f'state as it is either less than 0 or greater '
-                             f'than {max_possible_state_num}')
+            raise IndexError(f'The "from" state {from_st} is not a possible state as it is either less than 0 or '
+                             f'greater than {max_possible_state_num}')
         if (to_st < 0) or (to_st > max_possible_state_num):
-            raise IndexError(f'The "to" state {to_st} is not a possible state '
-                             f'as it is either less than 0 or greater than '
-                             f'{max_possible_state_num}')
+            raise IndexError(f'The "to" state {to_st} is not a possible state as it is either less than 0 or greater '
+                             f'than {max_possible_state_num}')
+
         for each_action in actions:
             if each_action == 'A':
                 action_list.append(Action(ActionType.APPEND))
